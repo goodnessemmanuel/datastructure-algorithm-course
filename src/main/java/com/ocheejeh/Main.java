4e44e3d3f03ca16fa287d61b27210a923f96581a
Oche Ejeh <oeejeh@unionbankng.com>
2022-03-07 14:14:47 +0100

    convert project to maven project

--- src/main/java/com/ocheejeh/Main.java
+++ src/main/java/com/ocheejeh/Main.java
@@ -3,5 +3,6 @@ package com.ocheejeh;
 public class Main {
     public static void main(String[] args) {
         //write your driver functions here...
+        System.out.println("Hello!");
     }
 }
